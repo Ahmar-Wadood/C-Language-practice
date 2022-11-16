8ca0f67e70b10c29918e8c55d1a7cf5a1f4a56d0
Ahmar Wadood <ahmarwadood@hotmail.com>
2022-11-16 16:10:10 +0500

    Initialization of Big projects

---
+++ BigProjects/studentRecordManagementSystem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//OBJECTIVE
+/* We have to make a student management system from where we can handle students records like(Student roll numbers, Name, Subjects)
+   We also have to make a database file where we can perform some operations like add, search, change and remove entries*/
+
+int main(){
+	
+	return 0;
+}
